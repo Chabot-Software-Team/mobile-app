9f43579042f7f87f53a27134d000a3456d046cbb
Aidan Sunbury <aidansunbury@gmail.com>
2021-06-25 14:52:35 -0700

    Started to format example page

--- pages/articles/Page1.js
+++ pages/articles/Page1.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 export default function Page1() {
   return(
-  <View>
-    <Text>Page1 </Text>
+  <ScrollView style = {styles.scrollView}>
+    <Text style = {styles.text}>Page1 Page1Page1Page1Page1Page1Page1 
+    Page1 Page1Page1Page1Page1Page1Page1Page1Page1
+    Page1 Page1 Page1 Page1 Page1 
+    </Text>
     
-  </View>
+  </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 40
+  },
+  scrollView: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
+});
