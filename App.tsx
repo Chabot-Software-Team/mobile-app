91cf9ad413c09469424fa0258bc2792214bb04b9
Rileyc2022 <rileyc2022@headroyce.org>
2022-02-20 14:50:18 -0800

    Fixed Futura font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import TabNavigation from "./components/TabNavigation";
 import * as Linking from "expo-linking";
+import { useFonts } from "expo-font";
 
 const prefix = Linking.createURL("/");
 
 export default function App() {
   const [data, setData] = useState(null);
+  const [loaded] = useFonts({
+    Futura: require("./assets/fonts/futura/Futura_Medium_bt.ttf"),
+  });
+
+
+
   const linking = {
     prefixes: [prefix],
     config: {
@@ -27,6 +34,7 @@ export default function App() {
       Linking.removeEventListener("url", handleDeepLink);
     };
   }, []);
-
+  
+  if (!loaded) return null;
   return <TabNavigation linking={linking} />;
 }
