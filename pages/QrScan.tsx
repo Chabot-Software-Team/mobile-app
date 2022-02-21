13c2d747ee5329f7e077883f0d5064500f718ae6
Max Andelman <s_max.andelman@ousd.org>
2022-02-20 16:00:38 -0800

    Studio2.tsk has been editied

--- pages/QrScan.tsx
+++ pages/QrScan.tsx
@@ -10,7 +10,7 @@ import { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 
 import { BarCodeScanner } from "expo-barcode-scanner";
-
+ 
 const styles = StyleSheet.create({
   button: {
     backgroundColor: "#2ed0cf",
