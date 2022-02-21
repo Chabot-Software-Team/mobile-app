07f3e789547373a6f3ceb834810dff2145b6112e
Arpita Pandey <arpitapandey@Arpitas-MacBook-Pro.local>
2022-02-20 16:02:46 -0800

    Edited Studio3.tsx

--- pages/globalStyles.tsx
+++ pages/globalStyles.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { StyleSheet, Dimensions } from "react-native";
 import { useFonts } from "expo-font";
-
+/*
 const [loaded] = useFonts({
   Futura: require("../assets/fonts/futur.ttf"),
 });
-
+*/
 const globalStyles = StyleSheet.create({
   articleImages: {
     width: Dimensions.get("window").width,
   },
-  headerText: {},
-  subHeaderText: {},
+  headerText: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  subHeaderText: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
   bodyText: {
     fontSize: 20,
   },
